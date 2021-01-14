c7c80edbb1588cb097038a8524c4eb0e780c9aa3
Manasvini Nagampalli <mnagamapalli506@student.fuhsd.org>
2021-01-14 13:09:33 -0800

    Tester complete!

--- GridWorldPart3/src/TesterD.java
+++ GridWorldPart3/src/TesterD.java
@@ -1,8 +1,16 @@
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Jumper;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Location;
 
 public class TesterD {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ActorWorld world = new ActorWorld();
+		world.add(new Location(0,0), new Jumper());
+		world.show();
 
 	}
 
