836b3bcc1151e532f2e658b8ff1ecf08294cdc42
Spencer Ye <sye348@student.fuhsd.org>
2021-01-14 13:09:11 -0800

    create tester A

--- GridWorldPart3/src/TesterA.java
+++ GridWorldPart3/src/TesterA.java
@@ -1,4 +1,22 @@
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Jumper;
+import info.gridworld.grid.Location;
 
-public class TesterA {
-
+/*
+ * What will a jumper do if the location in front of it is empty, but the location two cells in front contains a flower or a rock?
+ * - It will not jump and then turn 90 degrees to the right
+ */
+public class TesterA 
+{
+	public class TesterB 
+	{
+		public static void main(String[] args)
+		{
+			ActorWorld world = new ActorWorld();
+			world.add(new Location(4, 5), new Jumper());
+			world.add(new Location(4, 3), new Flower());
+			world.show();
+		}
+	}
 }
