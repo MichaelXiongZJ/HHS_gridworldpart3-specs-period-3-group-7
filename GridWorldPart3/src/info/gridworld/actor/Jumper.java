4baabc26619a11cfd8f042013d40de74e0baa2b2
Spencer Ye <sye348@student.fuhsd.org>
2021-01-14 12:57:33 -0800

    whee update

---
+++ GridWorldPart3/src/info/gridworld/actor/Jumper.java
@@ -0,0 +1,36 @@
+package info.gridworld.actor;
+
+import java.awt.Color;
+
+public class Jumper extends Actor
+{
+	public Jumper()
+	{
+		
+	}
+	
+	public Jumper(Color color)
+	{
+		
+	}
+	
+	public void act()
+    {
+        
+    }
+	
+	public boolean canMove()
+	{
+		return false;
+	}
+	
+	public void move()
+	{
+		
+	}
+	
+	public void turn()
+	{
+		
+	}
+}
