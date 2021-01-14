fe9192fff46460584d23885ef60210cf1c7e328f
Kuma_Sama <michaelx@users.noreply.github.com>
2021-01-14 13:15:23 -0800

    Tester E,F

--- GridWorldPart3/src/TesterE.java
+++ GridWorldPart3/src/TesterE.java
@@ -1,8 +1,17 @@
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Jumper;
+import info.gridworld.grid.Location;
 
+//What will a jumper do if it encounters another jumper in its path? 
 public class TesterE {
 
-	public TesterE() {
-		// TODO Auto-generated constructor stub
-	}
+	public static void main(String[] args)
+    {
+        ActorWorld world = new ActorWorld();
+        world.add(new Location(4, 5), new Jumper());
+        world.add(new Location(2, 5), new Jumper());
+        world.show();
+
+    }	
 
 }
