191cfd6e4fd681202413de6187e662c252440be1
Kuma_Sama <michaelx@users.noreply.github.com>
2021-01-14 13:41:54 -0800

    Update TesterF.java

--- GridWorldPart3/src/TesterF.java
+++ GridWorldPart3/src/TesterF.java
@@ -3,17 +3,18 @@ import info.gridworld.actor.Jumper;
 import info.gridworld.actor.Rock;
 import info.gridworld.grid.Location;
 
-//Are there any other tests the jumper needs to make? 
+//What if the jumper is surrounded by rocks that are one block away from it.? 
 public class TesterF {
 
 	public static void main(String[] args)
     {
         ActorWorld world = new ActorWorld();
-        world.add(new Location(4, 5), new Jumper());
-        world.add(new Location(4, 6), new Rock());
-        world.add(new Location(4, 4), new Rock());
+        world.add(new Location(5, 5), new Jumper());
+        world.add(new Location(5, 7), new Rock());
+        world.add(new Location(5, 3), new Rock());     
         world.add(new Location(3, 5), new Rock());
-        world.add(new Location(5, 5), new Rock());
+        world.add(new Location(7, 5), new Rock());
+        
         world.show();
 
     }	
