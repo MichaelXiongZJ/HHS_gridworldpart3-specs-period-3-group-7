8cfc71c9619dd131c2b8f1db3ba6bbc3ac0c099c
Kuma_Sama <michaelx@users.noreply.github.com>
2021-01-14 13:04:22 -0800

    N/A

--- GridWorldPart3/src/MichaelTester.java
+++ GridWorldPart3/src/MichaelTester.java
@@ -1,8 +1,1 @@
-
-public class MichaelTester {
-
 	public MichaelTester() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
---
+++ GridWorldPart3/src/TesterE.java
@@ -0,0 +1,8 @@
+
+public class TesterE {
+
+	public TesterE() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
