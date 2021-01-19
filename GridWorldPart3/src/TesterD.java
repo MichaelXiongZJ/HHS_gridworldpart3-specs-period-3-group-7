f99560f66335d107c7a4cbf35c8c93d3e99db8eb
Manasvini Nagampalli <mnagamapalli506@student.fuhsd.org>
2021-01-19 10:50:24 -0800

    fixed tester d to match the same direction

--- GridWorldPart3/src/TesterD.java
+++ GridWorldPart3/src/TesterD.java
@@ -7,10 +7,14 @@ import info.gridworld.grid.Location;
 public class TesterD {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		ActorWorld world = new ActorWorld();
-		world.add(new Location(1,2), new Jumper());
-		world.add(new Location(3,2), new Bug());
+		Jumper j = new Jumper();
+		Bug b = new Bug();
+		world.add(j);
+		world.add(b);
+		j.moveTo(new Location(1, 2));
+		b.moveTo(new Location(3, 2));
 		world.show();
 
 	}
